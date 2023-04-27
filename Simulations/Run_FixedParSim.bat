a4d319c66d8ba67cfee2b9aa598dde41a93449ab
AlistairBoettiger <boettiger.alistair@gmail.com>
2023-04-27 12:38:25 -0700

    added script based parameter scans for pc spreading model

--- Simulations/Run_FixedParSim.bat
+++ Simulations/Run_FixedParSim.bat
@@ -1,3 +1,2 @@
 call C:\ProgramData\anaconda3\Scripts\activate.bat  C:\ProgramData\anaconda3\
-activate openmm_env 
-python C:\Users\Alistair.BOETTIGERSERVER\Desktop\code\boettiger-notebook\PolymerSims\PolySimDemo_FixedPars.py
+activate openmm_env && python C:\Shared\polychrom-shared\Simulations\Example_3D_StateSpreading_FixedPars_Scan.py
